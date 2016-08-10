a84dfc31316371019d8ea26888a333ddb44e8c53
Gordon Child <gordon.child@ihg.com>
2016-08-10 17:46:43 -0600

    removed unnecessary files

--- src/main/java/com/gordonchild/websocket/chat/domain/server/PartyRoomInfo.java
+++
@@ -1,4 +0,0 @@
-package com.gordonchild.websocket.chat.domain.server;
-
-public class PartyRoomInfo {
-}
--- src/main/java/com/gordonchild/websocket/chat/domain/session/PartyRoomSession.java
+++
@@ -1,4 +0,0 @@
-package com.gordonchild.websocket.chat.domain.session;
-
-public abstract class PartyRoomSession extends RoomSession {
-}
