38f498447007b2f8d21beb4fab75ed4fb722af60
Gordon Child <gordon.child@ihg.com>
2016-08-10 17:45:47 -0600

    removed unnecessary files

--- src/main/java/com/gordonchild/websocket/chat/domain/request/party/CreatePartyRequest.java
+++
@@ -1,4 +0,0 @@
-package com.gordonchild.websocket.chat.domain.request.party;
-
-public class CreatePartyRequest {
-}
--- src/main/java/com/gordonchild/websocket/chat/domain/request/party/JoinPartyRequest.java
+++
@@ -1,7 +0,0 @@
-package com.gordonchild.websocket.chat.domain.request.party;
-
-public class JoinPartyRequest {
-
-
-
-}
